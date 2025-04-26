e27e4faeac0d49477af398ae5f36d697e47999ce
max-eisenberg <maxeisenberg@g.ucla.edu>
2025-04-26 14:54:28 -0700

    profile

--- App/market.ai/market.ai/Main/Settings.swift
+++ App/market.ai/market.ai/Main/Settings.swift
@@ -10,78 +10,121 @@ import SwiftUI
 struct ProfilePage: View {
     @State private var isDarkMode = true
     
+    @State private var portfolio: [Stock] = [
+        Stock(name: "Apple", ticker: "AAPL", marketCap: 3872409585350, iconUrl: URL(string: "https://api.polygon.io/v1/reference/company-branding/YXBwbGUuY29t/images/2025-04-04_icon.png")!),
+        Stock(name: "Microsoft", ticker: "MSFT", marketCap: 3133802247084, iconUrl: URL(string: "https://api.polygon.io/v1/reference/company-branding/bWljcm9zb2Z0LmNvbQ/images/2025-04-04_icon.png")!),
+        Stock(name: "Alphabet", ticker: "GOOGL", marketCap: 1876556400000, iconUrl: URL(string: "https://api.polygon.io/v1/reference/company-branding/YWxwaGFiZXQuY29t/images/2025-04-04_icon.png")!),
+        Stock(name: "Amazon", ticker: "AMZN", marketCap: 2005630000000, iconUrl: URL(string: "https://api.polygon.io/v1/reference/company-branding/YW1hem9uLmNvbQ/images/2025-04-04_icon.png")!),
+        Stock(name: "NVIDIA", ticker: "NVDA", marketCap: 2708640000000, iconUrl: URL(string: "https://api.polygon.io/v1/reference/company-branding/bnZpZGlhLmNvbQ/images/2025-04-04_icon.png")!),
+        Stock(name: "Berkshire Hathaway", ticker: "BRK.B", marketCap: 1145090000000, iconUrl: URL(string: "https://api.polygon.io/v1/reference/company-branding/YmVya3NoaXJlLmNvbQ/images/2025-04-04_icon.png")!),
+        Stock(name: "Tesla", ticker: "TSLA", marketCap: 917810000000, iconUrl: URL(string: "https://api.polygon.io/v1/reference/company-branding/dGVzbGEuY29t/images/2025-04-04_icon.png")!),
+        Stock(name: "Meta Platforms", ticker: "META", marketCap: 1448168302087, iconUrl: URL(string: "https://api.polygon.io/v1/reference/company-branding/bWV0YS5jb20/images/2025-04-04_icon.png")!),
+        Stock(name: "UnitedHealth Group", ticker: "UNH", marketCap: 418640000000, iconUrl: URL(string: "https://api.polygon.io/v1/reference/company-branding/dW5pdGVkaGVhbHRoZ3JvdXAuY29t/images/2025-04-04_icon.png")!),
+        Stock(name: "Johnson & Johnson", ticker: "JNJ", marketCap: 154580000000, iconUrl: URL(string: "https://api.polygon.io/v1/reference/company-branding/am5qLmNvbQ/images/2025-04-04_icon.png")!),
+    ]
+    
     var body: some View {
         ZStack {
             Color("bgNavy")
                 .ignoresSafeArea()
-
-            VStack(alignment: .leading, spacing: 50) {
+            
+            ScrollView {
                 
-                // Profile Card
-                HStack {
-                    Spacer()
-                    VStack(alignment: .center, spacing: 12) {
-                        Image("Logo")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 100, height: 100)
-                            .foregroundColor(.white.opacity(0.8))
-                        
-                        Text("Max Eisenberg")
-                            .font(.system(size: 26, weight: .bold))
-                            .foregroundColor(.white)
-                        
-                    }
-                    Spacer()
-                }
-
-                // Account Section
-                VStack(alignment: .leading, spacing: 15) {
-                    Text("Account")
-                        .font(.system(size: 20, weight: .bold))
-                        .foregroundColor(.white)
+                VStack(alignment: .leading, spacing: 30) {
                     
-                    VStack(spacing: 12) {
-                        profileRow(icon: "bookmark.fill", title: "Saved Articles")
-                        profileRow(icon: "person.2.fill", title: "Following")
-                        profileRow(icon: "bell.fill", title: "Notifications")
+                    // Profile Card
+                    HStack {
+                        Spacer()
+                        VStack(alignment: .center, spacing: 12) {
+                            Image("Logo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                                .foregroundColor(.white.opacity(0.8))
+                            
+                            Text("Max Eisenberg")
+                                .font(.system(size: 26, weight: .bold))
+                                .foregroundColor(.white)
+                            
+                        }
+                        Spacer()
                     }
-                }
-                
-                // App Settings Section
-                VStack(alignment: .leading, spacing: 15) {
-                    Text("Settings")
-                        .font(.system(size: 20, weight: .bold))
-                        .foregroundColor(.white)
                     
-                    VStack(spacing: 12) {
-                        Toggle(isOn: $isDarkMode) {
-                            HStack(spacing: 10) {
-                                Image(systemName: "moon.fill")
-                                    .foregroundColor(.gray)
-                                Text("Dark Mode")
-                                    .foregroundColor(.white)
+                    // individual stocks
+                    VStack(alignment: .leading, spacing: 20) {
+                        HStack {
+                            Text("Your Portfolio")
+                                .font(.system(size: 20, weight: .bold))
+                                .foregroundColor(.white)
+                            Spacer()
+                            Text("Edit")
+                                .font(.system(size: 16, weight: .medium))
+                                .foregroundColor(.gray)
+                                .padding(.trailing)
+                        }
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack(spacing: 15) {
+                                ForEach(portfolio, id: \.name) { stock in
+                                    StockPillFYP(
+                                        icon: stock.iconUrl!,
+                                        ticker: stock.ticker,
+                                        isSelected: false
+                                    )
+                                }
                             }
                         }
-                        .toggleStyle(SwitchToggleStyle(tint: .blue))
+                    }
+                    
+                    // Account Section
+                    VStack(alignment: .leading, spacing: 15) {
+                        Text("Your Market")
+                            .font(.system(size: 20, weight: .bold))
+                            .foregroundColor(.white)
                         
-                        profileRow(icon: "lock.fill", title: "Privacy Policy")
-                        profileRow(icon: "arrow.backward.circle.fill", title: "Sign Out")
+                        VStack(spacing: 12) {
+                            settingRow(icon: "heart.fill", title: "Favorite Articles")
+                            settingRow(icon: "message.badge.filled.fill", title: "Saved Chats")
+                        }
+                    }
+                    
+                    // App Settings Section
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Settings")
+                            .font(.system(size: 20, weight: .bold))
+                            .foregroundColor(.white)
+                        
+                        VStack(spacing: 12) {
+                            settingRow(icon: "lock.fill", title: "Privacy Policy")
+                            settingRow(icon: "arrow.backward.circle.fill", title: "Sign Out")
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Spacer()
+                        Text("Sign out")
+                            .font(.system(size: 18, weight: .medium))
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color("purpleLight"))
+                            .foregroundColor(.black)
+                            .cornerRadius(10)
+                        Spacer()
                     }
                 }
-                
-                Spacer()
+                .padding()
             }
-            .padding()
+            .scrollIndicators(.hidden)
         }
     }
     
-    private func profileRow(icon: String, title: String) -> some View {
+    private func settingRow(icon: String, title: String) -> some View {
         HStack(spacing: 12) {
             Image(systemName: icon)
                 .frame(width: 30, height: 30)
                 .foregroundColor(.white)
-
+            
             Text(title)
                 .foregroundColor(.white.opacity(0.9))
                 .font(.system(size: 16, weight: .medium))
